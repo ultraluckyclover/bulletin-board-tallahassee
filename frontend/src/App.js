b1fec1b4127bac3cfe488a2575bb64993b72ed07
Drew Goldstein <goldsteindrew314@gmail.com>
2025-02-16 13:56:34 -0500

    feat: FilterBar UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,9 @@
 
 import './App.css';
 import  Map  from './components/Map/Map'
-
 import { EventsList } from './components/EventsList/EventsList';
-import { BigEventCard } from './components/EventCards/BigEventCard';
-import { useState, useReact, useEffect } from 'react';
-import supabase from './supabaseClient';
-import { Filter } from './components/FilterBar/FilterBar';
-import { EventsContext, EventsProvider, useEvents } from './scripts/Context';
+import { FilterBar } from './components/FilterBar/FilterBar';
+import {  EventsProvider } from './scripts/Context';
 
 
 
@@ -19,10 +15,9 @@ const App = () => {
     <div className = 'flex justify-between relative'>
 
       <EventsProvider>
-
         <EventsList/>
         <Map />
-        <Filter/>
+        <FilterBar />
       </EventsProvider>
       
       
--- frontend/src/components/FilterBar/FilterBar.jsx
+++ frontend/src/components/FilterBar/FilterBar.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
+import './FilterBar.css';
 
-export const Filter = () => {
+export const FilterBar = () => {
   return (
     <div className = ' z-[300] absolute right-0 flex gap-x-10 p-4'>
-      <button className = 'border-green-400 border-2 p-2 rounded-full w-[100px] text-center bg-offwhite drop-shadow-2xl'>Type</button>
-      <button className = 'border-green-400 border-2 p-2 rounded-full w-[100px] text-center bg-offwhite'>Date</button>
+      <div class="select">
+        <div
+          class="selected"
+          data-default="All"
+          data-one="option-music"
+          data-two="option-literary"
+          data-three="option-art"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="1em"
+            viewBox="0 0 512 512"
+            class="arrow"
+          >
+            <path
+              d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"
+            ></path>
+          </svg>
+
+          <div class="main-box">
+            <span id="selected-option">Type</span>
+          </div>
+        </div>
+        <div class="options">
+          
+          <div title="option-music">
+            <input id="option-music" name="option" type="checkbox" />
+            <label class="option" for="option-music" data-txt="music"></label>
+          </div>
+          <div title="option-2">
+            <input id="option-literary" name="option" type="checkbox" />
+            <label class="option" for="option-literary" data-txt="literary"></label>
+          </div>
+          <div title="option-3">
+            <input id="option-art" name="option" type="checkbox" />
+            <label class="option" for="option-art" data-txt="art"></label>
+          </div>
+        </div>
+      </div>      
     </div>
   )
 }
